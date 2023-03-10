77681afa999dad31bbdb09fc2691b25daa90a5fa
zafar alam <113663507+zafargate@users.noreply.github.com>
2023-03-10 15:33:52 +0530

    2nd commit

---
+++ src/test/java/practice/Practice2.java
@@ -0,0 +1,27 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Practice2 {
+	@Test
+	void test1() {
+		
+		System.out.println("C2,test1===>Test abc");
+	}
+	@Test
+	void test2() {
+		System.out.println("C2,test2===>Test cde");
+	}
+	@Test
+	void test3() {
+		System.out.println("C2,test3===>Test efg");
+	}
+	@Test
+	void test4() {
+		System.out.println("C2,test3====>Test ghi");
+	}
+	@Test
+	void test5() {
+		System.out.println("C2,test5===>Test ijk");
+	}
+}
---
+++ src/test/java/practice/Pratic.java
@@ -0,0 +1,28 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class Pratic {
+	@Test
+	void test1() {
+		
+		System.out.println("C1,test1===>Test abc");
+	}
+	@Test
+	void test2() {
+		System.out.println("C1,test2===>Test cde");
+	}
+	@Test
+	void test3() {
+		System.out.println("C1,test3===>Test efg");
+	}
+	@Test
+	void test4() {
+		System.out.println("C1,test3====>Test ghi");
+	}
+	@Test
+	void test5() {
+		System.out.println("C1,test5===>Test ijk");
+	}
+
+}
